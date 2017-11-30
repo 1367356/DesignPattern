cd1d7794a1aa7dc74be9c9bae183f785480e918a
1367356 <1185978642@qq.com>
2017-11-30 21:33:34 +0800

    commit
    commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/83/00049becd2d500171059ae1c450d8d10
@@ -0,0 +1,14 @@
+package defaultAdapter;
+
+/**
+ * 一般的适配器为了改变源接口，使其适应目标接口。缺省适配器用意有所不同
+ * 缺省适配器，也就是子类（抽象类）实现一部分方法
+ * @author admin
+ *
+ */
+public interface SuperDefaultAdapter {
+	
+	public void a();
+	public int b();
+	public String c();
+}
---
+++ structuralType/src/adaptorPattern/defaultAdapter/SubDefaultAdapter.java
@@ -0,0 +1,21 @@
+package adaptorPattern.defaultAdapter;
+
+/**
+ * 缺省适配器，也就是子类（抽象类）实现一部分方法
+ * @author admin
+ *
+ */
+public abstract class SubDefaultAdapter implements SuperDefaultAdapter{
+
+	public void a() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int b() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
